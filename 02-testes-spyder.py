54eadf291ceafdb70508e2035ff84214e5aefcd1
Dagberto Rigue <riguealive@gmail.com>
2021-06-08 02:36:21 -0300

    Add files via upload

---
+++ 02-testes-spyder.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+Execute cada um dos trechos de teste abaixo
+
+@autor : dagbertoRigue
+"""
+# teste 1
+a = 1
+
+# teste 2
+import numpy as np
+np.max([3, 5, 78])
+
+# teste 3
+import sys
+sys.version
+
+# teste 4
+import tensorflow
+tensorflow.version
